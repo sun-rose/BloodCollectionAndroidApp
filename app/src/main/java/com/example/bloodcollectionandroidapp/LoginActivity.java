4979c03f4603d6764d3a52a5311b82ad4c06255c
hazzatun1 <hazzatun.jahan@gmail.com>
2024-06-19 15:21:45 +0600

    Login activity applied some functionalities.

--- app/src/main/java/com/example/bloodcollectionandroidapp/LoginActivity.java
+++ app/src/main/java/com/example/bloodcollectionandroidapp/LoginActivity.java
@@ -2,13 +2,153 @@ package com.example.bloodcollectionandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+   private Button signInButton,twitterSignInButton, facebookSignInButton;
+    private TextView signUpText,forgotPasswordText;
+    EditText mEmail, mPassword;
+    String email, password;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+         signInButton = findViewById(R.id.sign_in);
+         twitterSignInButton = findViewById(R.id.sign_in_twitter);
+         facebookSignInButton = findViewById(R.id.sign_in_facebook);
+         signUpText = findViewById(R.id.sign_up);
+         forgotPasswordText = findViewById(R.id.forgot_password);
+        forgotPasswordText = findViewById(R.id.forgot_password);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager conMgr =  (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
+                if (netInfo == null) {
+                    Toast.makeText(LoginActivity.this, "No Internet", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    email = mEmail.getText().toString().trim();
+                    password = mPassword.getText().toString().trim();
+
+                    if (TextUtils.isEmpty(email)) {
+                        mEmail.setError("Email is Required.");
+                        return;
+                    }
+//
+                    if (TextUtils.isEmpty(password)) {
+                        mPassword.setError("Password is Required.");
+                        return;
+                    }
+
+                    if (password.length() < 6) {
+                        mPassword.setError("Password Must be >= 6 Characters");
+                        return;
+                    }
+                    // authenticate the user
+
+                    fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete( Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+
+
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        twitterSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle Twitter sign in button click
+            }
+        });
+
+        facebookSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle Facebook sign in button click
+            }
+        });
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        forgotPasswordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(LoginActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure( Exception e) {
+                                Toast.makeText(LoginActivity.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+                passwordResetDialog.create().show();
+
+            }
+        });
+
     }
 }
